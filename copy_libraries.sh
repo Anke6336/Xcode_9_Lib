79cd9e08c37f3a3838a7224ab57208863fe2d7cc
zhanglexiao <380787537@qq.com>
2019-03-29 10:14:00 +0800

    For Xcode 10.12

---
+++ copy_libraries.sh
@@ -0,0 +1,3 @@
+cp ./Xcode10_needs/iPhoneOS/*.tbd /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib
+
+cp ./Xcode10_needs/iPhoneSimulator/*.tbd /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/lib
